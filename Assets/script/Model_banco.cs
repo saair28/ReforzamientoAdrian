4dba63aa1f333c9ed17945fd5c137adc542c1b25
Rodrigo <go.cormel@hotmail.com>
2020-07-17 19:47:13 -0500

    rodrigo

---
+++ Assets/script/Model_banco.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEditor.Experimental.AssetImporters;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+using UnityEngine.UI;
+
+public class Model_banco : MonoBehaviour
+{
+
+    public Text PanelIzqui;
+    public Text PanelCent;
+    public Text PanelDere;
+    public int contPanel;
+    public int turnos;
+    public bool siguiente = false;
+    public int a;
+    public int b;
+    public int c;
+    public GameObject espere;
+
+    public void Start()
+    {
+        turnos = 0;
+
+        espere.SetActive(false);
+
+    }
+
+    public void Update()
+    {
+
+    }
+}
